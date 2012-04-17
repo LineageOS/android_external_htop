d571252a888fe74467d75330514804f3cf8ff31a
Amit Pundir <amit.pundir@linaro.org>
2012-04-17 16:34:27 +0530

    add Android makefile
    Usage of htop on Android:
    export TERMINFO=/etc/terminfo
    export TERM=linux
    htop --no-color
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

---
+++ Android.mk
@@ -0,0 +1,33 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS:= \
+	-O2 -pedantic -Wall -std=c99 -rdynamic -D_XOPEN_SOURCE_EXTENDED -DSYSCONFDIR=\"/system/etc\"
+
+LOCAL_C_INCLUDES:= \
+	external/ncurses/include \
+	external/htop-0.9/plpa-1.3.2/src/libplpa
+
+LOCAL_SRC_FILES:= \
+	AvailableMetersPanel.c CategoriesPanel.c CheckItem.c \
+	ClockMeter.c ColorsPanel.c ColumnsPanel.c CPUMeter.c CRT.c DebugMemory.c \
+	DisplayOptionsPanel.c FunctionBar.c Hashtable.c Header.c htop.c ListItem.c \
+	LoadAverageMeter.c MemoryMeter.c Meter.c MetersPanel.c Object.c Panel.c \
+	BatteryMeter.c Process.c ProcessList.c RichString.c ScreenManager.c Settings.c \
+	SignalItem.c SignalsPanel.c String.c SwapMeter.c TasksMeter.c TraceScreen.c \
+	UptimeMeter.c UsersTable.c Vector.c AvailableColumnsPanel.c AffinityPanel.c \
+	HostnameMeter.c OpenFilesScreen.c \
+	plpa-1.3.2/src/libplpa/plpa_internal.h \
+	plpa-1.3.2/src/libplpa/plpa_api_probe.c \
+	plpa-1.3.2/src/libplpa/plpa_dispatch.c \
+	plpa-1.3.2/src/libplpa/plpa_runtime.c \
+	plpa-1.3.2/src/libplpa/plpa_map.c
+
+LOCAL_STATIC_LIBRARIES:= libncurses
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE:= htop
+
+include $(BUILD_EXECUTABLE)
